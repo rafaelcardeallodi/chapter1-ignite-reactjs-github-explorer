b9934e9c032318991cb33792ce460581bb006f2b
rafaelcardeallodi <rafaelcardeallodi@hotmail.com>
2021-10-18 08:44:33 -0300

    Listando repositórios

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -5,12 +5,6 @@ import '../styles/repositories.scss';
 
 // https://api.github.com/users/rafaelcardeallodi/repos
 
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/'
-}
-
 export function RepositoryList(){
   const [repositories, setRepositories] = useState([]);
 
@@ -25,10 +19,9 @@ export function RepositoryList(){
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />
+        })}
       </ul>
     </section>
   )
